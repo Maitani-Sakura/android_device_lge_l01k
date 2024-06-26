1d330caa223d3c4f56640299cdd61ef86e995011
Han Sol Jin <hansol@hansol.ca>
2024-06-26 15:07:56 -0700

    l01k: decommonize
    Change-Id: If8d1121d5ccd0d4a421cbfa38026612807afbc7b

--- lineage_l01k.mk
+++ lineage_l01k.mk
@@ -14,25 +14,28 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
-# Inherit from joan device
-$(call inherit-product, device/lge/joan/device.mk)
+# Inherit common repository
+$(call inherit-product, device/lge/joan-common/joan-common.mk)
+
+# Inherit proprietary blobs
+$(call inherit-product, vendor/lge/l01k/l01k-vendor.mk)
 
 # Device identifiers
-PRODUCT_DEVICE := joan
-PRODUCT_NAME := lineage_joan
+PRODUCT_DEVICE := l01k
+PRODUCT_NAME := lineage_l01k
 PRODUCT_BRAND := lge
-PRODUCT_MODEL := joan
+PRODUCT_MODEL := l01k
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RELEASE_NAME := V30
 
 PRODUCT_GMS_CLIENTID_BASE := android-om-lg
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE="joan" \
-    PRODUCT_DEVICE="joan" \
-    PRODUCT_NAME="joan_global_com" \
-    PRIVATE_BUILD_DESC="joan_global_com-user 8.0.0 OPR1.170623.026 181381736b4e9 release-keys"
+    TARGET_DEVICE="L-01K" \
+    PRODUCT_DEVICE="L-01K" \
+    PRODUCT_NAME="joan_dcm_jp" \
+    PRIVATE_BUILD_DESC="joan_dcm_jp-user 9 PKQ1.190414.001 20072184679ed release-keys"
 
-BUILD_FINGERPRINT="lge/joan_global_com/joan:8.0.0/OPR1.170623.026/181381736b4e9:user/release-keys"
+BUILD_FINGERPRINT="lge/joan_dcm_jp/L-01K:9/PKQ1.190414.001/20072184679ed:user/release-keys"
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model ro.vendor.product.model ro.product.system.model
