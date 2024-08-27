467d5e8ce504ab48608bf55ce3dabe98e739eff6
Michael Bestas <mkbestas@lineageos.org>
2024-08-27 22:05:28 +0300

    l01k: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc

--- extract-files.sh
+++ extract-files.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
 function blob_fixup() {
     case "${1}" in
-    vendor/lib*/vendor.lge.hardware.nfc@1.0.so|vendor/lib*/vendor.lge.hardware.nfc@1.1.so)
-        sed -i "s/libhidltransport\.so/libhidlbase_shim\.so/g" "${2}"
-        ;;
+        vendor/lib*/vendor.lge.hardware.nfc@1.0.so | \
+            vendor/lib*/vendor.lge.hardware.nfc@1.1.so)
+            [ "$2" = "" ] && return 0
+            sed -i "s/libhidltransport\.so/libhidlbase_shim\.so/g" "${2}"
+            ;;
+        *)
+            return 1
+            ;;
     esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 # If we're being sourced by the common script that we called,
